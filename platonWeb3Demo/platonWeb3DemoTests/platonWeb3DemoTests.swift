0556d3ae24e7ffe2efd0498cf6ad07321d01aefa
Ham Yu <yuhamgg@gmail.com>
2019-04-16 10:16:18 +0800

    add unit target

---
+++ platonWeb3Demo/platonWeb3DemoTests/platonWeb3DemoTests.swift
@@ -0,0 +1,33 @@
+//
+//  platonWeb3DemoTests.swift
+//  platonWeb3DemoTests
+//
+//  Created by Admin on 16/4/2019.
+//  Copyright © 2019 ju. All rights reserved.
+//
+
+import XCTest
+
+class platonWeb3DemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
