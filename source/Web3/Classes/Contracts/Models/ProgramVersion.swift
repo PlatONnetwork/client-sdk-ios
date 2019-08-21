423f4352ea67062765c9dbbaa6964e484f024638
Ham Yu <yuhamgg@gmail.com>
2019-08-21 21:17:47 +0800

    0.7.1开发

---
+++ source/Web3/Classes/Contracts/Models/ProgramVersion.swift
@@ -0,0 +1,14 @@
+//
+//  ProgramVersion.swift
+//  platonWeb3Demo
+//
+//  Created by Admin on 21/8/2019.
+//  Copyright © 2019 ju. All rights reserved.
+//
+
+import Foundation
+
+public struct ProgramVersion: Decodable {
+    var ProgramVersion: UInt32?
+    var ProgramVersionSign: String?
+}
